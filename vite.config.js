cce6fa93d51bb33583a8634e3e62eac595ef9afd
eubrunsllva-sudo <f.bruhsilva@gmail.com>
2026-03-15 16:46:25 -0300

    Fix: Ajuste de base path e build para deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Ajuste o 'base' para o nome do seu repositório exatamente como está no GitHub
+  // Exemplo: se o link é github.com/seu-user/meu-site, coloque '/meu-site/'
+  base: './', 
 })
